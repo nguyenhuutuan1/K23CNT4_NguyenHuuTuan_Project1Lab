9f07fd9d6c5abd40ef640faa4d700885aa2e8e9b
unknown <huutuansans@gmail.com>
2024-12-03 22:05:10 +0700

    nht-lesson01-update

---
+++ nht-lesson01/app/Http/Controllers/NamedProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NamedProfileController extends Controller
+{
+    public function display()
+    {
+        return "<h1> Named Route Controller </h1>";
+    }
+
+    public function show()
+    {
+        return redirect()->route('display.profile');
+    }
+}
